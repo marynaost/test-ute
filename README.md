9924a3f0183215be59c780fc9a28b6e0c51e778d
Maryna Ostapliuk <marynaum01@gmail.com>
2023-05-21 16:44:29 +0300

    1 version

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# test-ute
+# ute-test
---
+++ swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.mySwiper', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
